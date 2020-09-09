8238df155b8e8da380cdd1cd9b78d70004409d98
Jevon Tyler <jevont17@gmail.com>
2020-09-09 04:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ return string.toUpperCase()
 function whisper(string) {
 return string.toLowerCase()
 }
-function shout(string) {
-console.log() toUpperCase
+function logShout(string) {
+console.log().toUpperCase()
 }
